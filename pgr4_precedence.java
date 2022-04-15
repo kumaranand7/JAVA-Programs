a7f4fba7209013fccde91b273256e1972d2e3490
Anand Kumar Gupta <96008269+kumaranand7@users.noreply.github.com>
2022-04-15 18:50:41 +0530

    Add files via upload

---
+++ pgr4_precedence.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Scanner;
+public class pgr4_precedence {
+
+    public static void main(String[] args) {
+
+        int a=5;
+
+        System.out.println("a++");
+        System.out.println("a");
+        System.out.println("++a");
+        System.out.println("a++");
+
+    }
+}
