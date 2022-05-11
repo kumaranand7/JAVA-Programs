a7624a37cb59bdac8ada8c7bf0589b94568e79e4
Anand Kumar Gupta <96008269+kumaranand7@users.noreply.github.com>
2022-05-11 19:51:39 +0530

    Add files via upload

---
+++ Constructor1.java
@@ -0,0 +1,35 @@
+package com.company;
+
+class A
+{
+    A()
+    {
+        System.out.println("Contructor from Class A is called ");
+    }
+
+}
+class B extends A{
+    B()
+    {
+        super();
+        System.out.println("Contructor from Class B is called ");
+    }
+
+}
+
+
+public class Constructor1 {
+
+    public static void main(String[] args) {
+
+
+      B obj2=new B();    // Contructor from Class A is called
+                             //Contructor from Class B is called
+
+        A obj1=new A();    //Contructor from Class A is called
+
+
+
+
+    }
+}
