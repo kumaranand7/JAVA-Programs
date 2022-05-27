3394b15ddba1be9edbc5eecb08fe5af24cd8896a
Anand Kumar Gupta <96008269+kumaranand7@users.noreply.github.com>
2022-05-27 16:27:59 +0530

    Add files via upload

---
+++ java lab/assignment3_A.java
@@ -0,0 +1,34 @@
+package anand;
+
+public class Assignment3A {
+	  public static void main(String[] args)
+	  {
+	    
+
+		 try {
+		  int a=10,b=0;
+		  int c=a/b;
+		     System.out.println("value of c is"+c);
+		 }
+		 catch(Exception  e)
+		 {
+			 
+			 System.out.println(e);
+			 
+
+			  }
+		 finally {
+			 
+      System.out.println("The 'try catch' is finished.");
+
+			 
+		 }
+	  
+		  
+		  
+		  
+	  }
+	  
+	  
+	  
+	} 
