431286c36ceb7ddd8ca6d77950719aa661dbf9c0
Anand Kumar Gupta <kumarguptaanand1@gmail.com>
2026-02-08 18:37:08 +0530

    valid anagram leetcode

---
+++ Hunter/Leetcode/CheckAnagram.java
@@ -0,0 +1,27 @@
+package com.company.Hunter.Leetcode;
+
+import java.util.Arrays;
+
+public class CheckAnagram {
+
+    public boolean isAnagram(String s,String t){
+
+        if(s.length()!=t.length())
+            return false;
+
+        char [] Sh=s.toCharArray();
+        char [] Th=t.toCharArray();
+
+        Arrays.sort(Sh);
+        Arrays.sort(Th);
+
+        return Arrays.equals(Sh,Th);
+    }
+    public static void main(String[] args) {
+        String s="anagram";
+        String t="nagaram";
+        CheckAnagram obj = new CheckAnagram();
+        Boolean res=obj.isAnagram( s, t);
+        System.out.println(res);
+    }
+}
