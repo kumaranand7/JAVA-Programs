1bb1c033db1c5720953dc170f25e50685ca6fd0b
Anand Kumar Gupta <kumarguptaanand1@gmail.com>
2026-02-27 21:07:27 +0530

    trim in anagram

--- Hunter/Leetcode/CheckAnagram.java
+++ Hunter/Leetcode/CheckAnagram.java
@@ -6,11 +6,11 @@ public class CheckAnagram {
 
     public boolean isAnagram(String s,String t){
 
-        if(s.length()!=t.length())
-            return false;
+        char [] Sh=s.trim().toCharArray();
+        char [] Th=t.trim().toCharArray();
 
-        char [] Sh=s.toCharArray();
-        char [] Th=t.toCharArray();
+        if(Sh.length!=Th.length)
+            return false;
 
         Arrays.sort(Sh); 
         Arrays.sort(Th);
@@ -18,8 +18,8 @@ public class CheckAnagram {
         return Arrays.equals(Sh,Th);
     }
     public static void main(String[] args) {
-        String s="anagram";
-        String t="nagaram";
+        String s=" anagram ";
+        String t="nagaram ";
         CheckAnagram obj = new CheckAnagram();
         Boolean res=obj.isAnagram( s, t);
         System.out.println(res);
