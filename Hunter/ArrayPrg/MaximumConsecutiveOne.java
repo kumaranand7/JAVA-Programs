39269ac24a8d2300021f18d4570bc61033ecb661
Anand Kumar Gupta <kumarguptaanand1@gmail.com>
2026-04-06 21:35:26 +0530

    Maximum Consecutive One

---
+++ Hunter/ArrayPrg/MaximumConsecutiveOne.java
@@ -0,0 +1,27 @@
+package com.company.Hunter.ArrayPrg;
+
+public class MaximumConsecutiveOne {
+
+    public static void main(String[] args) {
+
+        int arr[]={1, 1, 0, 1, 1, 1};
+
+        int max=0;
+        int count=0;
+
+        for(int i =0; i<arr.length; i++){
+            if(arr[i]==1){
+                count++;
+            }
+            else {
+                max = Math.max(max, count);
+                count = 0;
+            }
+        }
+
+        max = Math.max(max, count);
+
+        System.out.println(max);
+
+    }
+}
