b8ecaf26f347a95825e09555dd7fb1398e879543
Anand Kumar Gupta <kumarguptaanand1@gmail.com>
2026-06-17 21:01:47 +0530

    check if even bit manipulation

--- Hunter/BitManipulation/BitManipulate.java
+++ Hunter/BitManipulation/BitManipulate.java
@@ -1,7 +1,8 @@
 package com.company.Hunter.BitManipulation;
 public class BitManipulate {
 
-    boolean isKthBitSet(int n, int k) {
+    boolean isKthBitSet(int n, int k) {     //if kth bit is set to 1
+
         int result = n & (1 << k) ;
         if (result != 0)
             return true;
@@ -11,10 +12,28 @@ public class BitManipulate {
         //return (n & (1 << k)) != 0; shortcut
     }
 
+
+    boolean isEven(int n){
+
+        //int res = n & (1 << 0);  this can be simplified as below
+
+        //int res = n & 1;
+        //return (res==0) ;   //this can be more simplified as below
+
+        return (n & 1) == 0;
+
+
+    }
     public static void main(String[] args) {
 
         BitManipulate b1 = new BitManipulate();
         System.out.println(b1.isKthBitSet(13,3)); //1101  //op true
+
+
+        System.out.println(b1.isEven(5));
+        System.out.println(b1.isEven(50));
+
+
 
     }
 }
